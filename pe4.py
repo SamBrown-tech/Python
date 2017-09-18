bda0d475b375e4dea0a79236df679a5a233215c5
Santino den Brave <s.denbrave@gillz.nl>
2017-09-18 21:10:44 +0200

    Final assignment 4 af

--- pe4.py
+++ pe4.py
@@ -20,7 +20,7 @@
 # print(lang_genoeg(180))
 
 # 4_4
-# def newPassword(oldpassword,newpassword):
+# def new_password(oldpassword,newpassword):
 #     if len(newpassword) >= 6 and oldpassword != newpassword:
 #         return "true"
 #     else: return "false"
@@ -37,22 +37,43 @@
 # lst = [-1,3,4,5]
 # print(kwadratenSom(lst))
 
+def kwadraten_som
 
 
 
 
 
 
-# 4 Final Assignment NS
-def standaardprijs(afstandKM):
-    if afstandKM > 50:
-        return 15 + (1.40 * afstandKM)
-    elif afstandKM <= 0:
-        return 0
-    else:
-        return 0.80 * afstandKM
-
-test = 60
-print("€" + "%.2f" % standaardprijs(test))
-
-def ritprijs(leeftijd, weekendrit, afstandKM):
+# # 4 Final Assignment NS
+# def standaardprijs(afstandKM):
+#     if afstandKM > 50:
+#         return 15 + (1.40 * afstandKM)
+#     elif afstandKM <= 0:
+#         return 0
+#     else:
+#         return 0.80 * afstandKM
+#
+# afstandKM = 60
+# print("€" + "%.2f" % standaardprijs(afstandKM))
+#
+# def ritprijs(leeftijd, weekendrit, afstandKM):
+#     # kinderen en ouderen korting
+#     if leeftijd  < 12 or leeftijd >= 65:
+#         # wel of geen weekend
+#         if weekendrit == "wel":
+#             return standaardprijs(afstandKM) * 0.65
+#         else: return standaardprijs(afstandKM) * 0.7
+#     # reizigers zonder korting
+#     else:
+#         #wel of geen weekend
+#         if weekendrit == "wel":
+#             return standaardprijs(afstandKM) * 0.6
+#         else: return standaardprijs(afstandKM)
+#
+#     # user input
+# afstandKM = int(input('Hoeveel kilometer? '))
+# leeftijd = int(input('Hoe oud bent u? '))
+# weekendrit = input('Reist u wel of niet in het weekend? ')
+#
+# # print("Uw ritprijs is €" + "0.2f" % str(ritprijs(leeftijd,weekendrit,afstandKM)))
+# print("Uw ritprijs is €" + str(ritprijs(leeftijd,weekendrit,afstandKM)))
